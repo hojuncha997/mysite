b5d353858e304eeab2311d501fe7d17719feeb69
bit306 <bit306@DESKTOP-UMBRTAG>
2021-01-08 01:03:16 +0900

    userController

--- src/main/java/bit/academy/mysite/repository/UserRepository.java
+++ src/main/java/bit/academy/mysite/repository/UserRepository.java
@@ -2,9 +2,59 @@ package bit.academy.mysite.repository;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+import bit.academy.mysite.vo.UserVo;
+
 public class UserRepository {
+	
+	public boolean insert(UserVo userVo) {
+		boolean result = false;
+
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		try {
+			conn = getConnection();
+			
+			// 3. SQL 준비
+			String sql =
+					" insert" +
+					"   into user" +
+					" values (null, ?, ?, ?, ?, now())";
+			pstmt = conn.prepareStatement(sql);
+			
+			// 4. 바인딩
+			pstmt.setString(1, userVo.getName());
+			pstmt.setString(2, userVo.getEmail());
+			pstmt.setString(3, userVo.getPassword());
+			pstmt.setString(4, userVo.getGender());
+			
+			// 5. sql문 실행
+			int count = pstmt.executeUpdate();
+			
+			result = count == 1;
+			
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} finally {
+			try {
+				// 3. 자원정리
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}		
+		
+		return result;
+	}
+	
+	
 	private Connection getConnection() throws SQLException{
 		Connection conn = null;
 		try {
@@ -20,4 +70,6 @@ public class UserRepository {
 		
 		return conn;
 	}
+
+	
 }
